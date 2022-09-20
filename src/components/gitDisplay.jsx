798f8b8256a696bd9e2001356c587c10ea80e976
Astagoth <jordandufour21@gmail.com>
2022-09-20 11:24:28 +0200

    Git display component
    Display git button to site

---
+++ src/components/gitDisplay.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class GitDisplay extends Component {
+    render() { 
+        return (
+            <div className="git-display">
+                <a href="https://github.com/Astagoth/Definitely-not-a-metroidvania" className="link">
+                    Repo GitHub
+                </a>
+            </div>
+        );
+    }
+}
+
+export default GitDisplay;
